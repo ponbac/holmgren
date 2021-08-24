d5da34bb547c354bd4885bd19e732b1105880a18
ponbac <ponbac@student.chalmers.se>
2021-08-25 01:41:08 +0200

    header items now properly centered

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -1,23 +1,29 @@
 import React from "react";
-import logo from "../../../public/images/logo_small.png";
+import logo from "../../../public/images/logo_big.png";
 
 export default function Header() {
   return (
     <header className="flex justify-center">
-      <nav className="container items-center mt-5">
-        <div className="pb-1 lg:pl-0 pl-8">
-          <img src={logo} alt="" />
+      <nav className="container flex items-center mt-5 justify-around">
+        <div className="w-44 object-contain lg:pl-0 pl-8">
+          <img src={logo} alt="Holmgrens bygg och service" />
         </div>
-        <ul className="hidden sm:flex flex-1 justify-end items-center gap-12 text-white uppercase text-s font-bold drop-shadow-xl">
-          <li className="cursor-pointer hover:text-gray-300">Hem</li>
-          <li className="cursor-pointer hover:text-gray-300">Våra projekt</li>
-          <li className="cursor-pointer hover:text-gray-300">Om oss</li>
-          <li className="cursor-pointer hover:text-gray-300">Kontakt</li>
-        </ul>
+        <div className="">
+          <ul className="hidden sm:flex flex-1 justify-end items-center gap-12 text-white uppercase text-s font-bold drop-shadow-xl">
+            <NavItem text='Hem' />
+            <NavItem text='Våra projekt' />
+            <NavItem text='Om oss' />
+            <NavItem text='Kontakt' />
+          </ul>
+        </div>
         <div className="flex sm:hidden flex-1 justify-end">
           <i className="text-2xl fas fa-bars"></i>
         </div>
       </nav>
     </header>
   );
+
+  function NavItem(props) {
+    return <li className="cursor-pointer hover:text-gray-300 hover:underline">{props.text}</li>;
+  }
 }
