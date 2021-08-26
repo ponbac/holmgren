79f29299e4502c6b2b9ef760062792fae9555886
ponbac <ponbac@student.chalmers.se>
2021-08-26 17:25:22 +0200

    began working on about

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 export default function About() {
   return (
-    <div>
-
+    <div className="flex justify-center">
+      <div className="grid grid-cols-2 gap-4 border-4">
+        <div>1</div>
+        <div>2</div>
+      </div>
     </div>
   );
 }
